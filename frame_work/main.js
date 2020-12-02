f5b929d81d30b4ab4377cf6aa4baf5da86e218a2
MilleniumY2Kay <47250775+MilleniumY2Kay@users.noreply.github.com>
2020-12-01 19:43:56 -0500

    car and stage placement complete

--- frame_work/main.js
+++ frame_work/main.js
@@ -1,31 +1,28 @@
 var stage;
-var ryu;
+var player;
 var car;
 
+var active = false;
+var left = true;
+
 function preload() {
-	stage = loadImage("assets/stage.png");
-	car = loadImage("assets/car/car_0.png");
+	stage = loadImage("assets/stage.png");//1260x787 pixels
+
+	car = loadImage("assets/car/car_0.png");//400x300 pixels
+	
+	player = loadImage("assets/akuma_spritesheet.png");
 }
 
 function setup() {
-	createCanvas(640,400);
-	background(0,100,0);
+	createCanvas(1260,787);
+	background(0);
 
-	var mgr = new SceneMangager ();
-	mgr.stage = stage;
-	mgr.wire();
-	mgr.showScene(intro);
 }
 
 function draw() {
-   
+   image(stage,0,0);
+
+   car.resize(600,0);
+   image(car,350,300);
 }
 
-function intro() {
-	this.setup = function() {
-
-	}
-	this.setup = function() {
-
-	}
-}
