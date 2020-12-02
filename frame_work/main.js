415cc3bae003bd026d47145782dc946a4ed5e740
MilleniumY2Kay <47250775+MilleniumY2Kay@users.noreply.github.com>
2020-12-02 01:08:05 -0500

    timer, score board, ryu idle animation

--- frame_work/main.js
+++ frame_work/main.js
@@ -1,7 +1,14 @@
 var stage;
-var player;
-var car;
+var player = [];
+var car = [];
 var intro;
+
+var car_hp = 1400;
+var car_form = 0;
+
+var score = 0;
+var timer = 40;
+var alert = 140;
 
 var active = false;
 var left = true;
@@ -10,11 +17,13 @@ var left = true;
 function preload() {
 	stage = loadImage("assets/stage.png");//1260x787 pixels
 
-	car = loadImage("assets/car/car_0.png");//400x300 pixels
-	
-	player = loadSpriteSheet("assets/akuma_intro1.png",88 ,114 ,18 );
-
-	intro = loadAnimation(player);
+	for (var i = 0; i < 14; i++){
+		car[i] = loadImage("assets/car/car_" + i + ".png");//400x300 pixels
+	}
+	for (var i = 0; i < 10; i++){
+		player[i] = loadImage("assets/old_sprites/ryu_stand/ryu_stand_" + i + ".png");
+	}
+	idle = loadAnimation(player[0], player[1],player[2],player[3],player[4],player[5]);
 }
 
 function setup() {
@@ -25,13 +34,33 @@ function setup() {
 
 function draw() {
    clear();
+   stage.resize(600,0);
    image(stage,0,0);
 
-   car.resize(600,0);
-   image(car,350,300);
+   textSize(40);
 
-   animation(intro, 200, 200);
+   fill(255,140,0);
+   text(timer+"s", 500, 30);
 
+   fill(255,140,0);
+   text(score+"pts",30,30);
+
+   if (frameCount % 60 ==0 && timer > 0 && car_hp > 0){
+   	timer --;
+   }
+   //car[car_form].resize(600,0);
+   image(car[car_form],150,50);
+
+   animation(idle, 120, 240);
 
 }
+
+function MousePressed() {
+	if (car_hp > 0){
+		car_hp -= 100;
+		car_form += 1;
+	}
+}
+
+
 
